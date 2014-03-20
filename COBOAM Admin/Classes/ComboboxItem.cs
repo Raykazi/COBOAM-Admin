e5abe360253b53caa66308e53a65da055fad7d09
Raykazi <webmaster@channelofblessings.com>
2014-03-20 08:44:23 -0400

    Class to give my ComboBox Items a value to be referenced.

---
+++ COBOAM Admin/Classes/ComboboxItem.cs
@@ -0,0 +1,14 @@
+﻿namespace COBOAM_Admin.Classes
+{
+    public class ComboBoxItem
+    {
+        public string Text { private get; set; }
+        public object Value { get; set; }
+
+        public override string ToString()
+        {
+            return Text;
+        }
+
+    }
+}
