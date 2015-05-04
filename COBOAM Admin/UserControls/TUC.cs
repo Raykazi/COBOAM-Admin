7fc38264d6f58b85e08f1c27f3edc6d9cf18d778
Raykazi <webmaster@channelofblessings.com>
2015-05-04 16:15:43 -0400

    Updated Mysql Connector, Added Dataset for Media Sales component.  Added Config.xml to project.

---
+++ COBOAM Admin/UserControls/TUC.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COBOAM_Admin.UserControls
+{
+    public class TUserControl : UserControl
+    {
+        public MemoryCache[] memoryCaches = new MemoryCache[200];
+    }
+}
